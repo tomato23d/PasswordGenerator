7bc14aef7bab2debc4acc1d0a6e383d1df64e697
tomato23d <anna.a.art@gmail.com>
2023-03-02 13:12:45 +1100

    script is updated for comments

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// source the input characters
 const numbers =    [1,2,3,4,5,6,7,8,9,0];
 
 const specialChs = ["!","@","#","$","%","^","&","*","(",")","_","+",",",".",".","-","/",";",
@@ -17,6 +18,8 @@ function writePassword() {
 var possibleChoices = [];
 var newPassword = "a"; 
 
+// prompt for user selection on type of characters to include 
+
 var optionNumbers       = window.confirm("Would you like to include numbers? OK for yes or Cancel for no ", "yes");
 var optionSpecialChs    = window.confirm("Would you like to include special characters? OK for yes or Cancel for no ", "yes");
 var optionLetters       = window.confirm("Would you like to include letters? OK for yes or Cancel for no ", "yes");
@@ -32,6 +35,7 @@ if (isNaN(defineLength)===true)
 if (optionNumbers === false && optionSpecialChs === false && optionLetters === false && optionLettersUC === false)
                         {alert ("please select at least one choice")};
 
+// assemble the password source on the basis of choices made
 if (optionNumbers===true){ 
                         possibleChoices = possibleChoices.concat(numbers);
 }
@@ -46,6 +50,8 @@ if (optionLettersUC===true){
 }
  console.log(possibleChoices);
 
+ //generate the password of required length
+
  for (  var x = 0; x < defineLength; x++) { 
         var index = Math.floor(Math.random() * possibleChoices.length);
         var generatePassword = possibleChoices[index];
@@ -58,10 +64,11 @@ console.log (newPassword);}
 console.log(newPassword.charAt(0));
 newPassword = newPassword.slice(1);
 console.log(newPassword);
-        
+
+//assign the generated password to html element
 var myDocpassword = document.querySelector("#password");
 myDocpassword.value = "Your secure password:  " + newPassword;}
         
-        
+//invoke the function and complete the exercise       
 generateBtn.addEventListener("click", writePassword);
         
